fb2947d0c07b97af843d1b815cd08446b04e2757
Cesar Gonzalez <gonz9682@stthomas.edu>
2023-12-15 12:37:44 -0600

    Changes I meant to push:
    Have an input box and 'Go' button for a user to type a location (lat/long coordinates, address, etc.)
    Map should update when location is entered and 'Go' button pressed
    Input box text should update with new location (lat/long coordinates or address) when map is panned/zoomed
    NOTE: updating once pan/zoom has ended is recommended - constantly updating this during a pan will overwhelm the system
    Use the Nominatim API (https://nominatim.org/release-docs/develop/api/Overview/Links to an external site. (Links to an external site.)) to convert between address and lat/long
    Clamp input values if lat/long is outside of St. Paul's bounding box

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@ import { reactive, ref, onMounted } from 'vue'
 
 let crime_url = ref('');
 let dialog_err = ref(false);
+let searchLocation = ref('');
+let locationInputRef = ref(null);
+let updateTimer = null;
 let map = reactive(
     {
         leaflet: null,
@@ -13,27 +16,27 @@ let map = reactive(
         },
         zoom: 12,
         bounds: {
-            nw: {lat: 45.008206, lng: -93.217977},
-            se: {lat: 44.883658, lng: -92.993787}
+            nw: { lat: 45.008206, lng: -93.217977 },
+            se: { lat: 44.883658, lng: -92.993787 }
         },
         neighborhood_markers: [
-            {location: [44.942068, -93.020521], marker: null},
-            {location: [44.977413, -93.025156], marker: null},
-            {location: [44.931244, -93.079578], marker: null},
-            {location: [44.956192, -93.060189], marker: null},
-            {location: [44.978883, -93.068163], marker: null},
-            {location: [44.975766, -93.113887], marker: null},
-            {location: [44.959639, -93.121271], marker: null},
-            {location: [44.947700, -93.128505], marker: null},
-            {location: [44.930276, -93.119911], marker: null},
-            {location: [44.982752, -93.147910], marker: null},
-            {location: [44.963631, -93.167548], marker: null},
-            {location: [44.973971, -93.197965], marker: null},
-            {location: [44.949043, -93.178261], marker: null},
-            {location: [44.934848, -93.176736], marker: null},
-            {location: [44.913106, -93.170779], marker: null},
-            {location: [44.937705, -93.136997], marker: null},
-            {location: [44.949203, -93.093739], marker: null}
+            { location: [44.942068, -93.020521], marker: null },
+            { location: [44.977413, -93.025156], marker: null },
+            { location: [44.931244, -93.079578], marker: null },
+            { location: [44.956192, -93.060189], marker: null },
+            { location: [44.978883, -93.068163], marker: null },
+            { location: [44.975766, -93.113887], marker: null },
+            { location: [44.959639, -93.121271], marker: null },
+            { location: [44.947700, -93.128505], marker: null },
+            { location: [44.930276, -93.119911], marker: null },
+            { location: [44.982752, -93.147910], marker: null },
+            { location: [44.963631, -93.167548], marker: null },
+            { location: [44.973971, -93.197965], marker: null },
+            { location: [44.949043, -93.178261], marker: null },
+            { location: [44.934848, -93.176736], marker: null },
+            { location: [44.913106, -93.170779], marker: null },
+            { location: [44.937705, -93.136997], marker: null },
+            { location: [44.949203, -93.093739], marker: null }
         ]
     }
 );
@@ -48,22 +51,24 @@ onMounted(() => {
         maxZoom: 18
     }).addTo(map.leaflet);
     map.leaflet.setMaxBounds([[44.883658, -93.217977], [45.008206, -92.993787]]);
+    map.leaflet.on("dragend", updateInputFromMap);
+    map.leaflet.on("zoomend", updateInputFromMap);
 
     // Get boundaries for St. Paul neighborhoods
     let district_boundary = new L.geoJson();
     district_boundary.addTo(map.leaflet);
     fetch('data/StPaulDistrictCouncil.geojson')
-    .then((response) => {
-        return response.json();
-    })
-    .then((result) => {
-        result.features.forEach((value) => {
-            district_boundary.addData(value);
+        .then((response) => {
+            return response.json();
+        })
+        .then((result) => {
+            result.features.forEach((value) => {
+                district_boundary.addData(value);
+            });
+        })
+        .catch((error) => {
+            console.log('Error:', error);
         });
-    })
-    .catch((error) => {
-        console.log('Error:', error);
-    });
 });
 
 
@@ -87,6 +92,65 @@ function closeDialog() {
         dialog_err.value = true;
     }
 }
+
+// Function to update input box text when map is panned/zoomed
+async function updateInput() {
+    try {
+        const response = await fetch(
+            `https://nominatim.openstreetmap.org/reverse?format=json&lat=${map.center.lat}&lon=${map.center.lng}`
+        );
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        const address = data.display_name;
+
+        searchLocation.value = address;
+    } catch (error) {
+        console.error('Error fetching address:', error);
+    }
+}
+
+function updateInputFromMap() {
+    // Clear existing timer
+    if (updateTimer) {
+        clearTimeout(updateTimer);
+    }
+
+    // Set a new timer to update the input box text once the movement (drag or zoom) has ended
+    updateTimer = setTimeout(() => {
+        if (locationInputRef.value) {
+            // Get the updated center after drag or zoom
+            const newCenter = map.leaflet.getCenter();
+            map.center.lat = newCenter.lat;
+            map.center.lng = newCenter.lng;
+
+            updateInput();
+        }
+    }, 500);
+}
+
+// Function to search for location and update the map
+async function searchAndSetLocation() {
+    const apiUrl = `https://nominatim.openstreetmap.org/search?format=json&q=${searchLocation.value}&limit=1`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+
+    if (data.length > 0) {
+        const location = data[0];
+        const lat = parseFloat(location.lat);
+        const lng = parseFloat(location.lon);
+
+        // Clamp input values if lat/long is outside of St. Paul's bounding box
+        map.center.lat = Math.min(map.bounds.nw.lat, Math.max(map.bounds.se.lat, lat));
+        map.center.lng = Math.min(map.bounds.se.lng, Math.max(map.bounds.nw.lng, lng));
+
+        map.leaflet.setView([map.center.lat, map.center.lng], 16);
+    }
+}
+
 </script>
 
 <template>
@@ -95,16 +159,22 @@ function closeDialog() {
         <label class="dialog-label">URL: </label>
         <input id="dialog-url" class="dialog-input" type="url" v-model="crime_url" placeholder="http://localhost:8000" />
         <p class="dialog-error" v-if="dialog_err">Error: must enter valid URL</p>
-        <br/>
+        <br />
         <button class="button" type="button" @click="closeDialog">OK</button>
     </dialog>
     <div class="grid-container ">
         <div class="grid-x grid-padding-x">
             <div id="leafletmap" class="cell auto"></div>
         </div>
+        <div class="grid-x grid-padding-x">
+            <label>Enter Location:</label>
+            <input v-model="searchLocation" @change="searchAndSetLocation" ref="locationInputRef" />
+            <button class="button" @click="searchAndSetLocation">Go</button>
+        </div>
+
     </div>
 </template>
-
+  
 <style>
 #rest-dialog {
     width: 20rem;
